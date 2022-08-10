84e8ba9ccae520ff43ea95c551261f9b886add7d
Germano Kuerten <germanokuerten@gmail.com>
2022-08-09 20:38:40 -0700

    testing onPress

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
+
+  const handlePress = () => console.log("txt clicked")
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello Mundão!!!</Text>
+      <Text numberOfLines={1} onPress={handlePress}>Hello Mundão!!!</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
