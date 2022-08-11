100031bf66316383472b27f799b3dec176a4499a
Germano Kuerten <germanokuerten@gmail.com>
2022-08-10 18:29:40 -0700

    ViewImageScreen component added

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 
 function ViewImageScreen(props) {
     return (
-        <Image source={require("../assets")}/>
+        <View style={styles.container}>
+            <View style={styles.closeIcon}></View>
+            <View style={styles.deleteIcon}></View>
+            <Image resizeMode='contain' style={styles.Image} source={require("../assets/background2.jpg")}/>
+        </View>
     );
 }
 
 export default ViewImageScreen;
+
+const styles = StyleSheet.create({
+    closeIcon: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#fc5c65',
+        top: 40,
+        left: 20,
+    },
+    container: {
+        backgroundColor: "#000",
+        flex: 1,
+    },
+    deleteIcon: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#4ecdc4',
+        position: 'absolute',
+        top: 40,
+        right: 20,
+    },
+    Image: {
+        width: '100%',
+        height: '100%',
+    }
+})
