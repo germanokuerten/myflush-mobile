0b92e85b75b48fdb035b3b3c86394707ec610a5f
Germano Kuerten <germanokuerten@gmail.com>
2022-08-12 15:33:35 -0700

    AccountScreen updated

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
 import React from "react";
-import { View, Text } from "react-native-web";
-import MessagesScreen from "./app/screens/MessagesScreen";
+import { View, Text } from "react-native";
 
-import Icon from "./app/components/Icon";
-import Screen from "./app/components/Screen";
-import ListItem from "./app/components/ListItem";
+import AccountScreen from "./app/screens/AccountScreen";
 
 
 export default function App() {
   return (
-    <Screen>
-      <ListItem 
-        title="My title"
-        ImageComponent={<Icon name="email" />}
-      />
-    </Screen>
+    <AccountScreen />
   );
 }
