fbc35924c0bda15d2ed0d32189d5dfffa60b4a7e
Germano Kuerten <germanokuerten@gmail.com>
2022-08-11 21:48:49 -0700

    ListingDetailsScreen updated

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native-web";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 
 export default function App() {
   return (
-    <ListingDetailsScreen />
+    <ViewImageScreen />
   );
 }
--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -2,19 +2,49 @@ import React from 'react';
 import { View, Image, StyleSheet } from 'react-native';
 import AppText from '../components/AppText';
 
+import ListItem from '../components/ListItem';
+import colors from '../config/colors';
+
 function ListingDetailsScreen(props) {
     return (
         <View>
             <Image style={styles.image} source={require('../assets/bathroom.jpg')}/>
-            <AppText>Clean Bathroom</AppText>
+            <View style={styles.detailsContainer}>
+                <AppText style={styles.title}>Clean Bathroom</AppText>
+                <AppText style={styles.price}>$1</AppText>
+
+                <View style={styles.userContainer}>
+                    <ListItem 
+                        image={require('../assets/mano.png')}
+                        title="Germano Kuerten"
+                        subTitle='3 Listings'
+                    />
+                </View>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    detailsContainer: {
+        padding: 20,
+    },
     image: {
         width: '100',
         height: 300,
+    },
+    price: {
+        color: colors.secondary,
+        fontWeight: "bold",
+        fontSize: 20,
+        marginVertical: 10,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "500",
+    },
+    userContainer: {
+        marginVertical: 30,
     }
 })
 
