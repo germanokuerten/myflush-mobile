cfc7d1af65dea61a097403f0b5dd8941c97cc9f9
Germano Kuerten <germanokuerten@gmail.com>
2022-08-12 14:40:24 -0700

    AccountScreen added to screens

---
+++ app/screens/AccountScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Screen from '../components/Screen';
+
+function AccountScreen(props) {
+    return (
+        <Screen></Screen>
+    )
+}
+
+export default AccountScreen;
