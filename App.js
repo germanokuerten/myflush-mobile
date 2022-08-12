46ad1fa60f2ec6c4e127844e22a96490743ef18b
Germano Kuerten <germanokuerten@gmail.com>
2022-08-12 14:14:57 -0700

    Icon component added

--- App.js
+++ App.js
@@ -2,10 +2,19 @@ import React from "react";
 import { View, Text } from "react-native-web";
 import MessagesScreen from "./app/screens/MessagesScreen";
 
+import Icon from "./app/components/Icon";
+import Screen from "./app/components/Screen";
 
 
 export default function App() {
   return (
-    <MessagesScreen />
+    <Screen>
+      <Icon 
+        name='email'
+        size={100}
+        backgroundColor='red'
+        iconColor='white'
+      />
+    </Screen>
   );
 }
--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -28,6 +28,7 @@ const initialMessages = [
 function MessagesScreen(props) {
 
     const [messages, setMessages] = useState(initialMessages);
+    const [refreshing, setRefreshing] = useState(false);
 
     const handleDelete = message => {
         // Delete the message from messagees array
@@ -51,6 +52,17 @@ function MessagesScreen(props) {
                 />
             )}
             ItemSeparatorComponent={ListItemSeparator}
+            refreshing={refreshing}
+            onRefresh={() => {
+                setMessages([
+                    {
+                        id: 2,
+                        title: 'T2',
+                        description: 'D2',
+                        image: require('../assets/mano.png')
+                    },
+                ])
+            }}
         />
         </Screen>
     );
