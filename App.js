4c4ceb5dc0aad6b3b201b2d535ce4bf9314bfc3d
Germano Kuerten <germanokuerten@gmail.com>
2022-08-13 00:26:27 -0700

    AppTextInput added

--- App.js
+++ App.js
@@ -1,24 +1,13 @@
 import React, { useState } from "react";
-import { View, Text, TextInput } from "react-native";
 
 import Screen from "./app/components/Screen";
+import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
-
-  const [firstName, setFirstName] = useState('')
   
   return (
     <Screen>
-      <Text>{firstName}</Text>
-      <TextInput
-        onChangeText={text => setFirstName(text)} 
-        placeholder="First Name" 
-        style={{
-          borderBottomColor: "#ccc",
-          borderBottomWidth: 1,
-        }}  
-      />
-        
+      <AppTextInput placeholder="Username" icon='email'/>
     </Screen>
   );
 }
