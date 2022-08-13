1d58d5029888e5a82f8c5781373398308a5a0622
Germano Kuerten <germanokuerten@gmail.com>
2022-08-13 12:42:36 -0700

    LoginScreen.js updated

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Image, StyleSheet } from 'react-native'
 
+import AppButton from '../components/AppButton';
+import AppTextInput from '../components/AppTextInput';
 import Screen from '../components/Screen';
 
 function LoginScreen(props) {
+
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+
     return (
-        <Screen>
+        <Screen style={styles.container}>
             <Image 
                 style={styles.logo}
                 source={require("../assets/myflush2.png")}/>
+            <AppTextInput 
+                autoCapitalize="none"
+                autoCorrect={false}
+                icon="email"
+                keyboardType="email-address"
+                onChangeText={text => setEmail(text)}
+                placeholder="Email"
+                textContentType="emailAddress"
+            />
+            <AppTextInput
+                autoCapitalize="none"
+                autoCorrect={false}
+                icon="lock"
+                onChangeText={text => setPassword(text)}
+                placeholder="Password"
+                secureTextEntry
+                textContentType="password"
+            />
+            <AppButton title="Login" onPress={() => console.log(email, password)}/>
         </Screen>
     );
 }
@@ -16,11 +41,14 @@ function LoginScreen(props) {
 export default LoginScreen;
 
 const styles = StyleSheet.create({
+    container: {
+        padding: 15,
+    },
     logo: {
         width: 80,
         height: 80,
         alignSelf: 'center',
         marginTop: 50,
-        marginBottom: 20,
+        marginBottom: 30,
     }
 })
